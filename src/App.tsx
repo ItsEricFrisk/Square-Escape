b5fdaaea837a24a9ac2c624281048835d2573c82
Eric Frisk <125310875+ItsEricFrisk@users.noreply.github.com>
2025-04-29 16:57:10 +0200

    Error message for small devices added

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,31 @@
-import Canvas from "./components/canvas/Canvas.tsx"
-import { Instructions } from "./components/instructions/Instructions.tsx"
+import { useEffect, useState } from "react";
+import Canvas from "./components/canvas/Canvas.tsx";
+import { Instructions } from "./components/instructions/Instructions.tsx";
+
 function App() {
+  const [isDesktop, setIsDesktop] = useState(true);
+
+  useEffect(() => {
+    if (window.innerWidth < 768) {
+      setIsDesktop(false);
+    }
+  }, []);
 
   return (
-    <main className="w-screen h-screen flex flex-col items-center justify-center gap-10">
-      <Instructions />
-      <Canvas />
-    </main>
-  )
+    <>
+      {isDesktop ? (
+        <main className="w-screen h-screen flex flex-col items-center justify-center gap-10">
+          <Instructions />
+          <Canvas />
+        </main>
+      ) : (
+        <div className="w-screen h-screen flex flex-col items-center justify-center gap-4 px-10">
+          <h1 className="text-2xl font-bold">Sorry!</h1>
+          <p className="text-lg text-center">This game is optimized for larger screens. Please switch to a bigger device.</p>
+        </div>
+      )}
+    </>
+  );
 }
 
-export default App
+export default App;
